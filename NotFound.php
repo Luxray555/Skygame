21ad101bc4132bbe0b03617975dab30c7da5b2f9
Luxray555 <100209081+Luxray555@users.noreply.github.com>
2022-04-08 00:09:44 +0200

    Add files via upload

---
+++ NotFound.php
@@ -0,0 +1,25 @@
+<?php
+    require_once "Setup/database.php";
+    session_start();
+    
+    require 'FonctionPHP/phpFunction.php';
+  
+    if(isset($_SESSION['idUtilisateur'])){
+      if ($_SESSION['timestamp']!=null) {
+        $_SESSION['timestamp'] = expiredSession($_SESSION['timestamp']);
+      }else{
+        $_SESSION['chargement'] = 'deconnexion par innactivité';
+        header('Location: Chargement.php');
+      }
+      $user = informationUser($_SESSION['idUtilisateur'],$bdd);
+    }
+?>
+<?php
+    $header=0;
+    include "header.php";
+?>
+<main class="main close">
+<?php
+    include "footer.php";
+?>
+</main>
