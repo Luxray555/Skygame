c6f7e3d171e7f7a9e6aa3bd0e77a7834ac3fb357
Luxray555 <100209081+Luxray555@users.noreply.github.com>
2022-04-10 18:42:35 +0200

    Add files via upload

--- FonctionPHP/EnvoieMailSupport.php
+++ FonctionPHP/EnvoieMailSupport.php
@@ -5,19 +5,22 @@ session_start();
 require '../FonctionPHP/phpFunction.php';
 require "../RessourceAPI/PHPMailer/PHPMailerAutoload.php";
 
-if(isset($_POST['email']) && isset($_POST['sujet']) && isset($_POST['description'])){
+if(isset($_POST['email']) && isset($_POST['sujet']) && isset($_POST['description']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
     $to   = 'skygameprojet@gmail.com';
     $from = 'skygameprojet@gmail.com';
-    $name = "no";
+    $name = $_POST['email'];
     $subj = $_POST['sujet'];
-    $msg = '<div style="background-color:#c9c9c9;padding:20px;">
-            <h1 style="text-align:center;margin-bottom:50px;color:white">Support</h1>
-            <p style="text-align:center;font-size:18px;color:white;"><span style="font-weight:bold;">Email : </span><span style="border:solid white 2px;padding:5px;border-radius:5px;">'.$_POST['email'].'</span></p>
-            <p style="text-align:center;font-size:18px;color:white;"><span style="font-weight:bold;">Sujet : </span><span style="border:solid white 2px;padding:5px;border-radius:5px;">'.$_POST['sujet'].'</span></p>
-            <p style="text-align:center;font-size:18px;color:white;"><span style="font-weight:bold;">Description : </span><span style="border:solid white 2px;padding:5px;border-radius:5px;">'.$_POST['description'].'</span></p>
+    $msg = '<div style="background-color:#c9c9c9;padding:20px; max-width:600px;min-width:200px;">
+            <h1 style="text-align:center;margin-bottom:50px;color:black">Support</h1>
+            <p style="font-size:18px;color:black;display:unset;width:100%;"><span style="font-weight:bold;">Email : </span><span style="padding:5px;">'.$_POST['email'].'</span></p><br><br>
+            <p style="font-size:18px;color:black;display:unset;width:100%;"><span style="font-weight:bold;">Sujet : </span><span style="padding:5px;">'.$_POST['sujet'].'</span></p><br><br>
+            <p style="font-size:18px;color:black;display:unset;width:100%;"><span style="font-weight:bold">Description : </span></p><br>
+            <p style="font-size:18px;color:black;display:inherit;border:solid black 2px;padding:5px;border-radius:5px;width: 100%">'.$_POST['description'].'</p>
         </div>';
     $error=smtpmailer($to,$from, $name ,$subj, $msg);
     $_SESSION['notif']="Le message a été envoyer au support.";
+}else{
+    $_SESSION['notif']="Erreur";
 }
 header("Location: ../Support.php");
-?>
+?>  
