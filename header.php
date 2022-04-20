ca326d7d0e4a9c882950aa65396cd0babb171c4e
Luxray555 <100209081+Luxray555@users.noreply.github.com>
2022-04-21 01:15:30 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -10,6 +10,7 @@ if(isset($page[$URI])){
 <head>
     <meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="Public/Images/icon/logo.ico" type="image/x-icon" />
     <link rel="apple-touch-icon" href="Public/Images/icon/logo.ico" type="image/x-icon"/>
     <title>
