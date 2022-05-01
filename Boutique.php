2f833876dcb2e39a2cd30855263f82df75664702
Luxray555 <100209081+Luxray555@users.noreply.github.com>
2022-05-01 23:00:51 +0200

    Add files via upload

--- Boutique.php
+++ Boutique.php
@@ -49,7 +49,7 @@ if(isset($_SESSION['idUtilisateur'])){
                                 <h2 class="euro">'.$key['prixSkycoin'].'</h2>
                                 <img src="Public/Images/icon/skyCoin.png" alt="Image Skycoin">
                                 <h2 class="skyCoin">'.$key['convertSkycoin'].'</h2>
-                                <span>+'.$key['bonusSkycoin'].' Bonus</span>
+                                <span class="bonus">+'.$key['bonusSkycoin'].' Bonus</span>
                                 <div class="button-buy">
                                     <input type="hidden" name="idSkycoin" value="'.$key['idSkycoin'].'">
                                     <input type="submit" value="';
@@ -61,7 +61,7 @@ if(isset($_SESSION['idUtilisateur'])){
 										}else{
 											echo'Expirer" disabled/>
 											</div>
-											<i>Réssayer demain.</i>';
+											<i>Réssayer dans <span></span></i>';
 										}
                                     }else{
                                         echo 'Acheter" />
@@ -91,6 +91,41 @@ if(isset($_SESSION['idUtilisateur'])){
     		notifJs($_SESSION['notif']);
         	unset($_SESSION['notif']);
       	}
+		if(isset($user['idUtilisateur']) && $user['lastRecompence'] != null || strtotime(date('Y-m-d',strtotime($user['lastRecompence'])))>=strtotime(date('Y-m-d'))){
+			echo '
+			function timeString(t){
+				if(t.toString().length<=1){
+					t="0"+t;
+				}
+				return t;
+			}
+			let compteur_recompence = document.querySelector("form i span");
+			let now_time= new Date();
+			now_time = now_time.getHours()*3600+now_time.getMinutes()*60+now_time.getSeconds()
+			let rebour = 86400-now_time;
+			if(rebour>=0){
+				var hour = Math.floor(rebour/3600),
+					minute = Math.floor((rebour%3600)/60),
+					second = Math.floor((rebour%3600)%60);
+					compteur_recompence.innerHTML = timeString(hour)+"h"+timeString(minute)+"m"+timeString(second)+"s" ;
+				rebour--;
+			}
+			let intervalId1 = setInterval(function(){
+				if(rebour>=0){
+					var hour = Math.floor(rebour/3600),
+						minute = Math.floor((rebour%3600)/60),
+						second = Math.floor((rebour%3600)%60);
+						compteur_recompence.innerHTML = timeString(hour)+"h"+timeString(minute)+"m"+timeString(second)+"s" ;
+					rebour--;
+				}else{
+					document.querySelector(".button-buy input[type='."'".'submit'."'".']").value = "Obtenir";
+					document.querySelector(".button-buy input[type='."'".'submit'."'".']").disabled = false;
+					document.querySelector("form i").innerHTML="";
+					clearInterval(intervalId1);
+				}
+			},1000)
+			';
+		}
     ?>
 	</script>
 	<script src="Public/js/Parallax.js"></script>
