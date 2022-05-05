be7c4abf05e8addabd37b0fa3cca863862a825be
Luxray555 <100209081+Luxray555@users.noreply.github.com>
2022-05-05 11:38:33 +0200

    Update Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -113,33 +113,17 @@
           <div>
             <img src="Public/Images/background/nous1.png">
             <h3>Votre vision, notre savoir faire</h3>
-            <p>rtzhsttrzghzreiuhguitzreg
-              trzhrzthtrzh
-              rzthtzrhtgzrhttzrhg
-              rtzhsttrzghzreiuhguitzreg
-              trzhrzthtrzh
-              rzthtzrhtgzrhttzrhg
-            </p>
+            <p>Toujours à l'écoute de nos clients, nous essayons de toujours vous satisfaire en mettant régulièrement le site à jour dans le but que les jeux les plus récent soient à votre disposition.</p>
           </div>
           <div>
             <img src="Public/Images/background/nous2.png">
             <h3>Vos données, notre écosystème</h3>
-            <p>rtzhsttrzghzreiuhguitzreg
-              trzhrzthtrzh
-              rzthtzrhtgzrhttzrhg
-              rtzhsttrzghzreiuhguitzreg
-              trzhrzthtrzh
-              rzthtzrhtgzrhttzrhg</p>
+            <p>Vous avez un libre accès à notre écosystème qui vous met à votre disposition un large choix de personnalisation de votre profil et toutes données personnelles stockées dans notre base de données sont sécurisés afin de protéger votre vie privée.</p>
           </div>
           <div>
             <img src="Public/Images/background/nous3.png">
-            <h3> Votre stratégie, notre expertise</h3>
-            <p>rtzhsttrzghzreiuhguitzreg
-              trzhrzthtrzh
-              rzthtzrhtgzrhttzrhg
-              rtzhsttrzghzreiuhguitzreg
-              trzhrzthtrzh
-              rzthtzrhtgzrhttzrhg</p>
+            <h3>Nos services, notre catalogue</h3>
+            <p>Le catalogue qui est mis à votre disposition est constamment mis à jour par notre équipe. Ce qui vous permet d'avoir toujours accès aux derniers jeux proposer dans le monde. De plus, les informations de chaque jeux sont modifiées en temps réel.</p>
           </div>
         </div>
       </div>
