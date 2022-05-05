78a629f924340ec3fd149fece4097d5dea4e3428
Luxray555 <100209081+Luxray555@users.noreply.github.com>
2022-05-05 13:27:27 +0200

    Add files via upload

--- Catalogue.php
+++ Catalogue.php
@@ -207,31 +207,8 @@
         ?>
     </main>
     </body>
+    <script type="text/javascript" src="Public/js/Catalogue.js"></script>
     <script type="text/javascript">
-
-        const filtre = document.querySelector(".filtre"),
-            filtre_list = document.querySelector(".filtreList");
-        let close1 = true;
-        filtre.onclick = function() {
-            if(close1){
-                filtre_list.style.height = "325px";
-                document.getElementById("bottom-hr").style.display = "flex";
-                setTimeout(() => {
-                    document.getElementById("bottom-hr").style.opacity = "1";
-                }, 100);
-                close1 = false;
-            }else{
-                document.getElementById("bottom-hr").style.opacity = 0;
-                setTimeout(() => {
-                    document.getElementById("bottom-hr").style.display = "none";
-                }, 300);
-                filtre_list.style.height = "0px";
-                close1 = true;
-            }
-        }
-</script>
-    <script type="text/javascript">
-        
         function undefinedCover(image){
             if(typeof(image) == "undefined"){
                 return "Public/Images/background/no-cover-image.png";
